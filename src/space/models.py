a2351a39ec8f69067114707483a82e68147e9818
Mikita Yeudakimau <nikevdokimovv@gmail.com>
2022-12-01 23:53:34 +0300

    models ready

--- src/space/models.py
+++ src/space/models.py
@@ -1,3 +1,27 @@
 from django.db import models
+from django.contrib.auth.models import User
 
-# Create your models here.
+class SpaceStation(models.Model):
+    RUNNING = "running"
+    BROKEN = "broken"
+    CONDITION_CHOICES = [
+        (RUNNING,"running"),
+        (BROKEN,"broken")
+    ]
+    name = models.CharField(max_length=50, unique=True)
+    condition = models.CharField(default=RUNNING)
+    date_creation = models.DateTimeField(auto_now_add=True)
+    date_broken = models.DateTimeField(blank=True)
+
+class Pointing(models.Model):
+    X= "x"
+    Y= "y"
+    Z= "z"
+    AXIS_CHOICES = [
+        (X,"Axis X"),
+        (Y, "Axis Y"),
+        (Z, "Axis Z"),
+    ]
+    user = models.ForeignKey(User,on_delete=models.CASCADE,related_name="pointing_user")
+    axis = models.CharField(choices=AXIS_CHOICES)
+    distance = models.IntegerField
